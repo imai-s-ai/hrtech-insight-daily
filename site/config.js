9f660567808dd15a0ea8572c1d4ef1469365fb3e
imai-s-ai <imai-s@dym.jp>
2025-12-24 17:55:49 +0900

    Update config.js

--- site/config.js
+++ site/config.js
@@ -1,2 +1,2 @@
-window.LIKE_API_BASE = "PUT_YOUR_GAS_WEBAPP_URL_HERE";
+window.LIKE_API_BASE = "https://script.google.com/macros/s/AKfycbxaQr1epE9QLMsKXRi9-tH3qe-uAzyUef-Xk0vsJA1dSyRmyMhtmzjuNaQfAVtyGDd6Xw/exec";
 window.SITE_TITLE = "HRTech Insight Daily";
